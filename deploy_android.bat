2d89e5a2761d9ad6e5a56584515175c01b89f46f
hxlh50k <hxlh50k@gmail.com>
2026-01-02 10:37:40 +0800

    rename trime

--- deploy_android.bat
+++ deploy_android.bat
@@ -1,11 +1,11 @@
 @echo off
 REM 部署 Rime 配置到 Android 设备
-REM 主要文件：tongwenfeng-custom.trime.yaml (主题+键盘), moqi_xh.custom.yaml (模糊规则)
+REM 主要文件：shouxin_18key.trime.yaml (主题+键盘), moqi_xh-trime.custom.yaml (模糊规则)
 
 adb push trime.custom.yaml /sdcard/rime
-adb push moqi_xh.custom.yaml /sdcard/rime
-adb push moqi_xh.schema.yaml /sdcard/rime
-adb shell rm /sdcard/rime/build/tongwenfeng-custom.trime.yaml
-adb push tongwenfeng-custom.trime.yaml /sdcard/rime
+adb push moqi_xh-trime.custom.yaml /sdcard/rime
+adb push moqi_xh-trime.schema.yaml /sdcard/rime
+adb shell rm /sdcard/rime/build/shouxin_18key.trime.yaml
+adb push shouxin_18key.trime.yaml /sdcard/rime
 
 adb shell am broadcast -a com.osfans.trime.deploy
