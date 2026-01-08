78ebab52f79ccbd337828999d95110a7a8993a40
hxlh50k <hxlh50k@gmail.com>
2026-01-08 22:18:43 +0800

    Fix

--- deploy_android.bat
+++ deploy_android.bat
@@ -3,6 +3,8 @@ REM 部署 Rime 配置到 Android 设备
 REM 主要文件：shouxin_18key.trime.yaml (主题+键盘), moqi_xh-trime.custom.yaml (模糊规则)
 REM Lua 文件：精确输入处理器和过滤器（依赖 sbxlm.lib）
 
+adb push default.yaml /sdcard/rime
+adb push default.custom.yaml /sdcard/rime
 REM 配置文件
 adb push trime.custom.yaml /sdcard/rime
 @REM adb push moqi_xh-trime.custom.yaml /sdcard/rime
